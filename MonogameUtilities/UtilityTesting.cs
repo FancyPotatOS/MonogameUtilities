55af10181215580c00b05e269675ccd330080689
ortiz <caleb@ortizfamily.ca>
2023-07-01 13:33:44 -0300

    Creates static mask demo.

--- MonogameUtilities/UtilityTesting.cs
+++ MonogameUtilities/UtilityTesting.cs
@@ -47,18 +47,6 @@ namespace MonogameUtilities
             GlobalData.StaticContentReference = Content;
             GlobalData.Cursor = Content.Load<Texture2D>("cursor");
 
-            Texture2D rainWorldTex = Content.Load<Texture2D>("rain_world");
-            Texture2D rainWorldMask = Content.Load<Texture2D>("rain_world_mask");
-            Hitbox maskBounds = new(10, 10, rainWorldTex.Width, rainWorldTex.Height);
-            StaticMask staticMaskElement = new(10, 10, rainWorldTex.Width, rainWorldTex.Height, null, rainWorldMask, maskBounds, 1);
-            ImageElement imgElement = new(10, 10, rainWorldTex.Width, rainWorldTex.Height, null, rainWorldTex);
-
-            Draggable dragElement = new(10, 10, rainWorldTex.Width, rainWorldTex.Height, null, false);
-
-            staticMaskElement.AddChild(imgElement);
-            dragElement.AddChild(staticMaskElement);
-            canvas.AddChild(dragElement);
-
             pixel = Content.Load<Texture2D>("pixel");
         }
 
