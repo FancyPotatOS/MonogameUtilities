fe4919df4f3161b0d0dc89e881951a45b96a0260
ortiz <caleb@ortizfamily.ca>
2023-07-01 14:52:57 -0300

    Adds start of XML parser.

---
+++ MonogameUtilities/Parsing/XmlParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MonogameUtilities.Parsing
+{
+    public class XmlParser
+    {
+        XmlNode root;
+
+        public XmlParser(XmlNode root)
+        {
+            this.root = root;
+        }
+    }
+}
